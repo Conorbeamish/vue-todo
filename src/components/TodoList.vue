755310d79053e45ac9cb9e93562092a027410615
Conor Beamish <conorbeamish@hotmail.co.uk>
2024-07-25 13:14:50 +0100

    Remove duplicate h1

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto p-4">
     <div class="flex items-center justify-between mb-4">
-      <h1 class="text-2xl font-bold">Your Todos:</h1>
+      <h2 class="text-2xl font-bold">Your Todos:</h2>
 
       <!-- Filter dropdown -->
       <div class="flex items-center space-x-3">
